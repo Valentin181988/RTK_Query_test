ce6dc13389237e6c02e80ceb1a603719b5ec8a67
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-07-17 16:51:23 +0200

    PhoneBook form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { PhoneBookTitle } from './components/ContactsTitle/PhoneBookTitle';
+import { ContactsTitle } from './components/ContactsTitle/ContactsTitle';
 import { Contact } from './components/Contact';
-import { CreateContact } from './components/createContact';
+import { PhoneBookForm } from './components/PhoneBookForm/PhoneBookForm';
+
 
 function App() {
   return (
     <>
-      <CreateContact />
+      <PhoneBookTitle title="Phone book"/>
+      <PhoneBookForm />
+      <ContactsTitle title="Contacts"/>
       <Contact />
     </>
   );
